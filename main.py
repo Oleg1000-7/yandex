deb1d6162064b1fea8ead74f75bb048693a4affd
Oleg <oalzeg09@gmail.com>
2025-03-14 20:03:01 +0300

    request 2 fixed

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ db_session.global_init(f"db/{input()}")
 
 db_sess = db_session.create_session()
 
-colonists = db_sess.query(User).filter(User.address == "module_1",
-                                       User.speciality.notilike("%engineer%"), User.position.notilike("%engineer%"))
+colonists = db_sess.query(User.id).filter(User.address == "module_1",
+                                          User.speciality.notilike("%engineer%"),
+                                          User.position.notilike("%engineer%")).all()
 
 for colonist in colonists:
-    print(colonist)
+    print(colonist[0])
