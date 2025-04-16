60b31c19b7490569c23190cf5ee20210ad9b389e
Oleg <oalzeg09@gmail.com>
2025-04-16 18:42:55 +0300

    task_7*

--- data/users.py
+++ data/users.py
@@ -1,14 +1,15 @@
 import datetime
 import sqlalchemy
+from flask_login import UserMixin
 from sqlalchemy.orm import relationship
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from data.db_session import SqlAlchemyBase
 
-
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement="auto")
     surname = sqlalchemy.Column(sqlalchemy.String)
     name = sqlalchemy.Column(sqlalchemy.String)
     age = sqlalchemy.Column(sqlalchemy.Integer)
@@ -23,3 +24,11 @@ class User(SqlAlchemyBase):
 
     def __repr__(self):
         return f"<Colonist> {self.id} {self.surname} {self.name}"
+
+    def set_password(self, password: str):
+        self.hashed_password = generate_password_hash(password)
+        print(self.hashed_password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
+
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from os.path import exists
 from datetime import datetime
+from sqlalchemy.orm import Session
 
 from data import db_session
 from data.jobs import Jobs
 from data.users import User
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_login import LoginManager, login_user, logout_user, login_required
+
+from forms.login_form import LoginForm
+from forms.register_form import RegisterForm
 
 
-def create_db(db_sess):
+def create_db(db_sess: Session):
     user_list = [
         User(
             surname="Scott",
@@ -72,6 +77,8 @@ def create_db(db_sess):
 
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'secret_key'
 
 if not exists("db/database.db"):
@@ -83,10 +90,66 @@ else:
     db_sess = db_session.create_session()
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return db_sess.query(User).get(user_id)
+
+
 @app.route("/")
 def index():
     jobs = db_sess.query(Jobs).all()
     return render_template("works_log.html", jobs_list=enumerate(jobs, start=1))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+                surname = form.surname.data,
+                name = form.name.data,
+                age = form.age.data,
+                position = form.position.data,
+                speciality = form.speciality.data,
+                address = form.address.data,
+                email = form.email.data,
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 if __name__ == '__main__':
